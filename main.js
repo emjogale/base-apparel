171b482c99d632ae29287f501c1a5cb2f31378d0
Emma Gale <emmajgale@hotmail.com>
2021-07-19 17:10:43 +0100

    change style of arrow and start to add in hover styles

---
+++ main.js
@@ -0,0 +1,8 @@
+const enter = document.getElementById("enter");
+console.log(enter);
+
+// define the checkInput function
+const checkInput = () => {
+	console.log("here we check if the email is in the correct format");
+};
+enter.addEventListener("click", checkInput);
