efc5b2b3003602122c6778bf7839fac861871521
Emma Gale <emmajgale@hotmail.com>
2021-07-25 17:19:57 +0100

    adding in function to check if email is valid

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 const enter = document.getElementById("enter");
 console.log(enter);
+const input = document.querySelector("input");
+console.log(input);
+const email = document.querySelector(".email");
 
 // define the checkInput function
+// function emailIsValid (email) {
+//   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+// }
 const checkInput = () => {
-	console.log("here we check if the email is in the correct format");
+	console.log(input.value);
+	if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input.value)) {
+		console.log("it's a correct email!!");
+	} else {
+		console.log("not a correct email address");
+		const err = document.createElement("p");
+		err.innerHTML = "Please provide a valid email";
+		err.style.color = "red";
+		email.appendChild(err);
+	}
 };
 enter.addEventListener("click", checkInput);
