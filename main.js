251aea05aa2d829cd2ef02c76f7adbc9964c864e
Emma Gale <emmajgale@hotmail.com>
2021-07-25 18:15:48 +0100

    add in error exclamation mark

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ console.log(enter);
 const input = document.querySelector("input");
 console.log(input);
 const email = document.querySelector(".email");
-
+const error = document.querySelector(".error");
+console.log(error);
+const errorAlert = document.getElementById("error-alert");
+console.log(errorAlert);
 // define the checkInput function
 // function emailIsValid (email) {
 //   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
@@ -12,12 +15,17 @@ const checkInput = () => {
 	console.log(input.value);
 	if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input.value)) {
 		console.log("it's a correct email!!");
+		errorAlert.style.display = "none";
+		error.innerHTML = "";
 	} else {
 		console.log("not a correct email address");
-		const err = document.createElement("p");
-		err.innerHTML = "Please provide a valid email";
-		err.style.color = "red";
-		email.appendChild(err);
+		// const err = document.createElement("p");
+		// err.innerHTML = "Please provide a valid email";
+		// err.style.color = "red";
+		// email.appendChild(err);
+		error.innerHTML = "Please provide a valid email";
+		error.style.color = "red";
+		errorAlert.style.display = "block";
 	}
 };
 enter.addEventListener("click", checkInput);
