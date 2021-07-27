c449606c00b336eeb4dfda4cf7e5bd50dee0b0ec
Emma Gale <emmajgale@hotmail.com>
2021-07-27 15:17:09 +0100

    correcting the default part of the checkInput function

--- main.js
+++ main.js
@@ -1,28 +1,21 @@
+// define all the variables to use
 const form = document.querySelector(".email");
-console.log(form);
 const input = document.querySelector("input");
-console.log(input);
-const email = document.querySelector(".email");
 const error = document.querySelector(".error");
-console.log(error);
 const errorAlert = document.getElementById("error-alert");
-console.log(errorAlert);
+
 // define the checkInput function
-// function emailIsValid (email) {
-//   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
-// }
 const checkInput = (e) => {
+	e.preventDefault();
 	if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input.value)) {
 		console.log("it's a correct email!!");
 		errorAlert.style.display = "none";
 		error.innerHTML = "";
-		e.preventDefault();
 	} else {
 		console.log("not a correct email address");
 		error.innerHTML = "Please provide a valid email";
 		error.style.color = "red";
 		errorAlert.style.display = "block";
-		e.preventDefault();
 	}
 };
 form.addEventListener("submit", checkInput);
